b0bb3ab1a21b3c4ac7ef40cb49ae61bb356e1b0c
Taylor Stemple <taylorstemple@gmail.com>
2017-12-22 18:52:52 -0800

    chord now plays two chords

--- app.js
+++ app.js
@@ -22,8 +22,8 @@ var hats = new Tone.MetalSynth({
 frequency  : 1200,
 envelope  : {
 attack  : 0.001 ,
-decay  : 0.2 ,
-release  : 0.2
+decay  : 0.1 ,
+release  : 0.1
 }  ,
 harmonicity  : 5.1 ,
 modulationIndex  : 32 ,
@@ -55,12 +55,19 @@ var loopThree = new Tone.Loop(function(time){
 
 }, "1m")
 
+var loopFour = new Tone.Loop(function(time){
+  polySynth.triggerAttackRelease(['D4', 'F4', 'A4', 'C5'], "16n", time)
+
+}, "1m")
+
 document.querySelector('.loop3').addEventListener('change', function(e){
   if (e.target.checked){
     loopThree.start(0)
+    loopFour.start('0:3')
 
   } else {
     loopThree.stop(0)
+    loopFour.stop(0)
   }
 })
 
