711b177ebe4c0ec3b9b9de9492f86ea30dd36adc
Taylor Stemple <taylorstemple@gmail.com>
2017-12-22 16:55:40 -0800

    added global transport and second loop

--- app.js
+++ app.js
@@ -1,20 +1,57 @@
+
+//kickDrum
+//************************************************************************
+
 var kickDrum = new Tone.MembraneSynth().toMaster()
 
-
-
 var loopOne = new Tone.Loop(function(time){
-	kickDrum.triggerAttackRelease("C0", "4n", time)
+	kickDrum.triggerAttackRelease("C1", "4n", time)
 }, "4n")
 
+document.querySelector('.loop1').addEventListener('change', function(e){
+  if (e.target.checked){
+    loopOne.start(0)
+
+  } else {
+    loopOne.stop(0)
+  }
+})
+//hi hats
+//*************************************************************************
+var hats = new Tone.MetalSynth().toMaster()
+
+var loopTwo = new Tone.Loop(function(time){
+	hats.triggerAttackRelease("8n", time)
+}, "8n")
+
+
+//Play slash pause. actually really activates global transport
+//************************************************************************
+
+document.querySelector('.loop2').addEventListener('change', function(e){
+  if (e.target.checked){
+    loopTwo.start(0)
+
+  } else {
+    loopTwo.stop(0)
+  }
+})
 
 
 
-loopOne.start(0)
 
 
-document.querySelector('.toggle_loop').addEventListener('change', function(e){
-	if (e.target.checked){
-		Tone.Transport.start('+0.1')
+
+
+
+//set the transport to repeat
+Tone.Transport.loopEnd = '1m'
+Tone.Transport.loop = true
+
+//start/stop the transport
+document.querySelector('.globalTransport').addEventListener('change', function(e){
+  if (e.target.checked){
+    Tone.Transport.start('+0.1')
   } else {
     Tone.Transport.stop()
   }
