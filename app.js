1cd40fe17fc30ee2c53080909e34785200b36368
Taylor Stemple <taylorstemple@gmail.com>
2017-12-29 09:48:05 -0800

    another arp loop

--- app.js
+++ app.js
@@ -32,6 +32,8 @@ var chorusChord = new Tone.Chorus()
 var delayArp = new Tone.FeedbackDelay("8n", 0.5)
 
 var filterDrone = new Tone.AutoFilter().start();
+
+var bassVibrato = new Tone.Vibrato()
 //kickDrum
 //************************************************************************
 
@@ -233,11 +235,13 @@ document.querySelector('.chord_1').addEventListener('change', function(e){
 
 document.querySelector('.chord_2').addEventListener('change', function(e){
   if (e.target.checked){
-    chordTwo.start(0)
+    chordTwo.start(0);
+    droneLoop.start(0);
 
 
   } else {
-    chordTwo.stop(0)
+    chordTwo.stop(0);
+    droneLoop.stop(0);
 
   }
 })
@@ -259,7 +263,13 @@ var arpPart = new Tone.Part(function(time, note){
 	arp.triggerAttackRelease(note, "16n", time);
 }, [[0, "C5"], ["0:0:2", "E5"], ["0:1", "C5"],["0:1:2","E5"],["0:2","C5"],["0:2:2","E5"],["0:3","B5"],["0:3:2","G5"]]);
 
+var arpPart2 = new Tone.Part(function(time, note){
+	//this shows how to assign division in a specific part
+	arp.triggerAttackRelease(note, "16n", time);
+}, [[0, "C5"], ["0:0:2", "E5"], ["0:1", "G5"],["0:1:2","B5"],["0:2","C6"],["0:2:2","B5"],["0:3","G5"],["0:3:2","E5"]]);
+
 arpPart.loop = true;
+arpPart2.loop = true;
 
 document.querySelector('.arp_1').addEventListener('change', function(e){
   if (e.target.checked){
@@ -267,6 +277,15 @@ document.querySelector('.arp_1').addEventListener('change', function(e){
 
   } else {
     arpPart.stop(0)
+  }
+})
+
+document.querySelector('.arp_2').addEventListener('change', function(e){
+  if (e.target.checked){
+    arpPart2.start(0)
+
+  } else {
+    arpPart2.stop(0)
   }
 })
 //bass drone
@@ -286,7 +305,7 @@ var droneLoop = new Tone.Part(function(time, note){
 
 var bass = new Tone.DuoSynth()
 
-bass.chain(crushBass, globalReverb, Tone.Master)
+bass.chain(crushBass, bassVibrato, globalReverb, Tone.Master)
 
 
 bass.volume.value = 0;
@@ -426,8 +445,140 @@ document.querySelector('#bass_vol').addEventListener('input', function(e){
 	bass.volume.value = parseFloat(e.target.value)
 })
 
+document.querySelector('#vib_bass').addEventListener('input', function(e){
+	bassVibrato.frequency.value = parseFloat(e.target.value)
+})
+
+document.querySelector('#drone_filter').addEventListener('input', function(e){
+	filterDrone.frequency.value = parseFloat(e.target.value)
+})
+
 crushBass.bits = 8;
 
 document.querySelector('#crush_bass').addEventListener('input', function(e){
   crushBass.bits = parseFloat(e.target.value)
 })
+
+//######################EXPERIMENT#############################################
+
+let octave = 4;
+
+const keys = [];
+let prevKey = 0;
+
+const Instruments = {
+  // https://github.com/stuartmemo/qwerty-hancock
+  keyboard: {
+    // Lower octave.
+    a: 'Cl',
+    w: 'C#l',
+    s: 'Dl',
+    e: 'D#l',
+    d: 'El',
+    f: 'Fl',
+    t: 'F#l',
+    g: 'Gl',
+    y: 'G#l',
+    h: 'Al',
+    u: 'A#l',
+    j: 'Bl',
+    // Upper octave.
+    k: 'Cu',
+    o: 'C#u',
+    l: 'Du',
+    p: 'D#u',
+    ';': 'Eu',
+    "'": 'Fu',
+    ']': 'F#u',
+    '\\': 'Gu',
+  },
+};
+
+let instrument = Instruments.keyboard;
+
+const keyToNote = key => {
+  const note = instrument[ key ];
+  if ( !note ) {
+    return;
+  }
+
+  return Tone.Frequency(
+    note
+      .replace( 'l', octave )
+      .replace( 'u', octave + 1 )
+  ).toNote();
+};
+
+const onKeyDown = (() => {
+  let listener;
+
+  return synth => {
+    document.removeEventListener( 'keydown', listener );
+
+    listener = event => {
+      const { key } = event;
+
+      // Only trigger once per keydown event.
+      if ( !keys[ key ] ) {
+        keys[ key ] = true;
+
+        const note = keyToNote( key );
+        if ( note ) {
+          synth.triggerAttack( note );
+          prevKey = key;
+        }
+      }
+    };
+
+    document.addEventListener( 'keydown', listener );
+  };
+})();
+
+const onKeyUp = (() => {
+  let listener;
+  let prev;
+
+  return synth => {
+    // Clean-up.
+    if ( prev ) {
+      prev.triggerRelease();
+    }
+
+    document.removeEventListener( 'keyup', listener );
+
+    prev = synth;
+    listener = event => {
+      const { key } = event;
+      if ( keys[ key ] ) {
+        keys[ key ] = false;
+
+        const note = keyToNote( key );
+        if ( synth instanceof Tone.PolySynth ) {
+          synth.triggerRelease( note );
+        } else if ( note && key === prevKey ) {
+          // Trigger release if this is the previous note played.
+          synth.triggerRelease();
+        }
+      }
+    };
+
+    document.addEventListener( 'keyup', listener );
+  };
+})();
+
+// Octave controls.
+document.addEventListener( 'keydown', event => {
+  // Decrease octave range (min: 0).
+  if ( event.key === 'z' ) { octave = Math.max( octave - 1, 0 ); }
+  // Increase octave range (max: 10).
+  if ( event.key === 'x' ) { octave = Math.min( octave + 1, 9 ); }
+});
+
+// Init.
+(() => {
+  const synth = new Tone.PolySynth( 10 );
+  synth.toMaster();
+
+  onKeyDown( synth );
+  onKeyUp( synth );
+})();
